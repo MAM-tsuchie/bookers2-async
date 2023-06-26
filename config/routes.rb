4dadb032c3eb97ba4bab5d22429206d6c8e7e34d
TsuchieShinya <89980497+MAM-tsuchie@users.noreply.github.com>
2023-06-26 15:02:28 +0000

    [Finish] 1-6

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,11 @@ Rails.application.routes.draw do
   get "search" => "searches#search"
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resources :book_comments, only: [:create, :destroy]
-    resource :favorites, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy] do
+      get :toggle, on: :member
+    end
   end
+
   resources :users, only: [:index,:show,:edit,:update] do
     get :follows, on: :member
     get :followers, on: :member
